adfacb69efbb2668f2fc4f4c7db60a870264459d
Mina <mhcsc01@gmail.com>
2021-02-19 22:53:58 -0500

    Searching\Linear search - python

--- Algorithms/Searching/Linear Search/Main.java
+++ Algorithms/Searching/Linear Search/Main.java
@@ -1,20 +1,20 @@
 class Main {
-  public static int search(int arr[], int x) {
+  public static int linearSearch(int arr[], int x) {
     int n = arr.length;
     for (int i = 0; i < n; i++) {
       if (arr[i] == x) {
         return i;
-      }      
+      }
     }
     return -1;
   }
- 
+
   public static void main(String[] args) {
     int arr[] = { 2, 3, 4, 10, 40 };
     int x = 10;
 
     // Function call
-    int result = search(arr, x);
+    int result = linearSearch(arr, x);
     if (result == -1) {
       System.out.print("Element is not present in array" + "\n");
     } else {
